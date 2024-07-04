c5069166e8ece7bbb88c7efd32cbc22fb3028b16
Airej Tashfeen <airejtashfeen@gmail.com>
2024-07-05 02:58:43 +0500

    Added new solution

--- (M)findthesmallestdivisorgivenathreshold.cpp
+++ (M)findthesmallestdivisorgivenathreshold.cpp
@@ -7,29 +7,31 @@ using namespace std;
 
 class Solution {
 private:
-    int calculateSum(vector<int>& nums, int mid){
-        int sum = 0;
-        for(int i = 0; i < nums.size(); i++){
-            sum += ceil((double)nums[i] / mid);
-        }
-        return sum;
+int calculateSum(vector<int>& nums, int mid){
+    int sum=0;
+    for(int i=0; i< nums.size(); i++){
+        //Replacement for ceil function
+       sum = sum +  (nums[i]+ mid-1)/mid;
     }
 
+    return sum;
+}
 public:
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int low = 1;
-        int high = *max_element(nums.begin(), nums.end());
-        int ans = 0;
+        int low=1;
+        int high= *max_element(nums.begin(), nums.end());
+        int ans=0;
 
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            int currentSum = calculateSum(nums, mid);
+        while(low<=high){
+            int mid= low + (high-low)/2;
+            int currentSum= calculateSum(nums, mid);
 
-            if(currentSum <= threshold){
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+            if(currentSum<=threshold){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low= mid+1;
             }
         }
         return ans;
