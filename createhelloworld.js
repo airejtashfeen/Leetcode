ea523ad9dddb821052c22862a886961a2b26af41
Airej Tashfeen <airejtashfeen@gmail.com>
2024-07-08 15:49:52 +0500

    Added a new Js solution

---
+++ createhelloworld.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        const string= "Hello World";
+        return string;
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
