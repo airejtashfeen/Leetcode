41aa54b75ea8dd3ae0d69fc73aa33f67cd3ccabf
Airej Tashfeen <airejtashfeen@gmail.com>
2024-07-15 17:19:47 +0500

    Added a new linked list solution

---
+++ (M)odd_even_linkedlist.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head) return head;
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+
+        while (even != NULL && even->next != NULL) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
+
+// Helper function to print the list
+void printList(ListNode* head) {
+    ListNode* temp = head;
+    while (temp != nullptr) {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+// Main function to test the oddEvenList method
+int main() {
+    // Creating a test list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    // Printing original list
+    cout << "Original List: ";
+    printList(head);
+
+    // Creating Solution object and testing the method
+    Solution solution;
+    ListNode* result = solution.oddEvenList(head);
+
+    // Printing the result list
+    cout << "Modified List: ";
+    printList(result);
+
+    return 0;
+}
