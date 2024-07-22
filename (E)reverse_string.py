65d38c258d75c371d9b68bf250009d03a6d2882f
Airej Tashfeen <airejtashfeen@gmail.com>
2024-07-22 20:09:53 +0500

    Added same solution in C++ and Python

---
+++ (E)reverse_string.py
@@ -0,0 +1,10 @@
+#CRAZY LMAO
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+        
