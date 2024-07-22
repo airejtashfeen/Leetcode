c617fc2f9de0d1d02b03d7c9940e3e8ee8021188
Airej Tashfeen <airejtashfeen@gmail.com>
2024-07-22 13:53:24 +0500

    Added new SQL solution

---
+++ (M)department_highest_salary.sql
@@ -0,0 +1,11 @@
+
+SELECT 
+d.name AS Department,
+e.name AS Employee,
+e.salary as Salary
+FROM Department d, Employee e
+WHERE 
+e.departmentId= d.id AND (e.departmentId, salary) IN
+
+(SELECT departmentId, MAX(salary) FROM Employee GROUP BY
+departmentId);
