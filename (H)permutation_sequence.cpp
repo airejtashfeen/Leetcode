11b3a388093bb65654b76711c953d8c995683c95
Airej Tashfeen <airejtashfeen@gmail.com>
2024-08-11 00:49:59 +0500

    added hard solution

---
+++ (H)permutation_sequence.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        int fact = 1;
+        vector<int> numbers;
+
+        for (int i = 1; i < n; i++) {
+            fact = fact * i;
+            numbers.push_back(i);
+        }
+        numbers.push_back(n);
+        k--;
+        string ans = "";
+        while (true) {
+            ans = ans + to_string(numbers[k / fact]);
+            numbers.erase(numbers.begin() + k / fact);
+
+            if (numbers.size() == 0) break;
+
+            k = k % fact;
+            fact = fact / numbers.size();
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution sol;
+    int n = 3; // Number of elements
+    int k = 3; // k-th permutation
+
+    string permutation = sol.getPermutation(n, k);
+    cout << "The " << k << "-th permutation of numbers from 1 to " << n << " is: " << permutation << endl;
+
+    return 0;
+}
